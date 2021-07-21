0366e2753cb6ac2dae1fe34b53b581a458ad7d73
Igor Aleshin <iealeshi@mts.ru>
2021-07-21 14:23:51 +0300

    Added routing

---
+++ ElectroluxCodingExercise/ElectroluxCodingExercise/Router/MainScreenRouterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MainScreenRouterProtocol.swift
+//  ElectroluxCodingExercise
+//
+//  Created by Алешин Игорь Эдуардович on 21.07.2021.
+//
+
+import Foundation
+
+protocol MainScreenRouterProtocol {
+    func openDetails(with model: PhotoModel)
+}
